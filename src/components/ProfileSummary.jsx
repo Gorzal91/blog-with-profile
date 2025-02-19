cab7c1177902ea87e6829149493ef93f7ee4ad33
Artur Gorzalczynski <gorzal1991@gmail.com>
2025-02-19 17:47:57 -0600

    Initial commit

--- src/components/ProfileSummary.jsx
+++ src/components/ProfileSummary.jsx
@@ -4,9 +4,12 @@ const ProfileSummary = ({ data }) => {
     console.log('data from ProfileSummary:', data)
   return (
     <div className="profile-summary">
+      <div className="profile-header">
       { data?.imageData ?
         <div className="profile-picture">
+        <a href={data?.imageData?.link} target="_blank" rel="noopener noreferrer">
         <img src={data?.imageData?.imgSrc} alt={data?.imageData?.alt} />
+        </a>
       </div> :
       <p style={{border: '3px solid pink'}}>Imagedata is missing</p>
       }
@@ -14,8 +17,35 @@ const ProfileSummary = ({ data }) => {
         <h4>{data.userData.name}</h4>
         <p>{data.userData.bio}</p>
       </div>
+      </div>
+      
+      <div className="posts-section">
+        <h3>
+        <ul>
+        {data.posts.map((post, index) => (
+          <li key={index}>
+            <h4>{post.title}</h4>
+            <p>{post.article}</p>
+          </li>
+        ))}
+        </ul>
+        </h3>
+      </div>
+      
+      <div className="header-section">
+        <h3>
+        <p>{data.header}</p>
+        </h3>
+      </div>
+      
+      <div className="footer-section">
+        <h6>
+          <p>{data.footer}</p>  
+        </h6>
+      </div>  
     </div>
-  );
+      
+    );
 };
 
 export default ProfileSummary;
